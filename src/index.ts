0fff74d3530cf48da0ccf2c32adb8fd4fb06ec49
Emil Shafeev <amil0512@gmail.com>
2024-02-16 14:15:10 +0500

    base behaviors

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export const behaviorFns: Record<
+  string,
+  (
+    paramPrice: number,
+    productSize: { width: number; height: number; depth: number },
+    paramValue: number
+  ) => number
+> = {
+  'W*H*Param_cost': (paramPrice, size) => size.width * size.height * paramPrice,
+  'Price per inclusion': (paramPrice) => paramPrice,
+  'W*H*Param_cost*Param_value': (paramPrice, size, paramValue) =>
+    size.width * size.height * paramPrice * paramValue,
+}
